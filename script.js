b315ef4286f857efabb76a1675b1d6bcfdc9ccb1
MoonAlya04 <alyamoonbaek@gmail.com>
2025-04-02 21:15:54 +0400

    Added slider changes

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
-let api_key = "api_key=450de2ccb3594f7792ac2434c91755ce";
-let img_url_original = "https://image.tmdb.org/t/p/original";
-let img_url = "https://image.tmdb.org/t/p/w500";
+// let api_key = "api_key=450de2ccb3594f7792ac2434c91755ce";
+// let img_url_original = "https://image.tmdb.org/t/p/original";
+// let img_url = "https://image.tmdb.org/t/p/w500";
 
-fetch("https://api.themoviedb.org/3/movie/popular?" + api_key)
-    .then((response) => response.json())
-    .then((response) => printProducts(response.results))
-    .catch((err) => console.error(err));
+// fetch("https://api.themoviedb.org/3/movie/popular?" + api_key)
+//     .then((response) => response.json())
+//     .then((response) => printProducts(response.results))
+//     .catch((err) => console.error(err));
 
-function printProducts(products) {
-    let container = document.querySelector(".__container");
-    container.innerHTML = "";
+// function printProducts(products) {
+//     let container = document.querySelector(".__container");
+//     container.innerHTML = "";
 
-    products.forEach((product) => {
-        let div = document.createElement("div");
-        div.classList.add("product");
+//     products.forEach((product) => {
+//         let div = document.createElement("div");
+//         div.classList.add("product");
 
-        div.innerHTML = `
-            <img src="${img_url_original + product.backdrop_path}" alt="${
-            product.title}">
-            <h2>${product.title}</h2>
-            <p>${product.overview}</p>
-            <p>Rating: ${product.vote_average}</p>
-            <p>Release Date: ${product.release_date}</p>
-        `;
+//         div.innerHTML = `
+//             <img src="${img_url_original + product.backdrop_path}" alt="${
+//             product.title}">
+//             <h2>${product.title}</h2>
+//             <p>${product.overview}</p>
+//             <p>Rating: ${product.vote_average}</p>
+//             <p>Release Date: ${product.release_date}</p>
+//         `;
 
-        container.appendChild(div);
-    });
-}
+//         container.appendChild(div);
+//     });
+// }
